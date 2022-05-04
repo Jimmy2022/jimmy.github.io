9a910c4663e9a07e299f2ef8aacef451e27e2585
jimmy <17718603+Jimmy2022@users.noreply.github.com>
2022-05-04 11:51:07 +0800

    feat: add more search engine and beautify battery

--- js/buttom.js
+++ js/buttom.js
@@ -1,13 +1,11 @@
-<script>
-    // this must after search-content
-    // https://www.tutorialspoint.com/adding-default-search-text-to-search-box-in-html-with-javascript
-    var defaultSearchText = "??";
-    var textBoxSearch = document.getElementById("search-content");
-    textBoxSearch.value = defaultText;
-    textBoxSearch.onfocus = function () {
-        if (this.value == defaultSearchText) {
+// this must after search-content
+// https://www.tutorialspoint.com/adding-default-search-text-to-search-box-in-html-with-javascript
+var defaultSearch = "日历";
+var textBoxSearch = document.getElementById("search-content");
+textBoxSearch.value = defaultSearch;
+textBoxSearch.onfocus = function () {
+    if (this.value == defaultSearch) {
         this.value = ''
-            console.log("focuse on search box")
-        }
+        console.log("focuse on search box")
     }
-</script>
+}
--- js/widgets.js
+++ js/widgets.js
@@ -1,57 +1,247 @@
-<script>
-	window.onload = function() {
-		// https://blog.csdn.net/animatecat/article/details/85164093
-		var isCharge;
-		var batteryPercent = 0;
-		navigator.getBattery().then(function(battery) {
-			isCharge = battery.charging;
-			batteryPercent = Math.round(battery.level * 100);
-			var batteryInfoStyle;
-			if (batteryPercent >= 85) {
-				batteryInfoStyle = "b-green"
-			} else if (batteryPercent > 30) {
-				batteryInfoStyle = "b-orange"
-			} else {
-				batteryInfoStyle = "b-red"
-			}
-			if (!batteryPercent) {
-				batteryPercent = "未知";
-				batteryInfoStyle = "b-orange"
-			}
-			if (isCharge) {
-				style = "b-green"
-			}
-			var content = " 电池电量:" + batteryPercent + "%"content = (isCharge ? "正在充电": "未在充电") + content
+//    https://baijiahao.baidu.com/s?id=1707034727158253423&wfr=spider&for=pc
+var SEARCH_ENGINE_MAP = new Map();
+// type, searchUrl, icoUrl, hot
+SEARCH_ENGINE_MAP.set('baidu', {
+	'main': 'https://baidu.com',                     // required
+	'search': 'https://www.baidu.com/s?word={0}',    // optional
+	'explor': ['https://top.baidu.com/board?tab=realtime'],                                    // optional
+	'icon': {
+		'url': 'https://www.baidu.com/favicon.ico',  // required
+		'pc': 1,                                   // optional
+		'mobile': 1,                               // optional
+	}
+});
+SEARCH_ENGINE_MAP.set('bing_cn', {
+	'url': 'https://www.bing.com',
+	'search': 'https://cn.bing.com/search?q={0}',
+	'explor': [],
+	'icon': {
+		'url': 'https://cn.bing.com/sa/simg/favicon-2x.ico',
+		'pc': 0.95,
+	}
+});
+SEARCH_ENGINE_MAP.set('yandex', {
+	'url': 'https://yandex.com',
+	'search': 'https://yandex.com/search/?text={0}',
+	'explor': [],
+	'icon': {
+		'url': 'https://avatars.mds.yandex.net/get-bunker/56833/d6c72510e34955b7c7fef9fd3af8f71a88763081/orig'
+	}
+});
+SEARCH_ENGINE_MAP.set('zhihu', {
+	'url': 'https://zhihu.com',
+	'search': 'https://www.zhihu.com/search?type=content&q={0}',
+	'explor': ['https://www.zhihu.com/explore'],
+	'icon': {
+		'url': 'https://static.zhihu.com/heifetz/favicon.ico',
+		'pc': 0.90,
+		'mobile': 0.8
+	}
+});
+SEARCH_ENGINE_MAP.set('google', {
+	'url': 'https://www.google.com',
+	'search': 'https://www.google.com/search?q={0}',
+	'explor': [],
+    'icon': {
+		'url': 'http://www.google.cn/favicon.ico',
+		'pc': 1.1,
+	}
+});
+SEARCH_ENGINE_MAP.set('github', {
+	'url': 'https://www.github.com',
+	'search': 'https://github.com/search?q={0}',
+	'explor': ['https://github.com/explore', 'https://github.com/trending'],
+	'icon': {
+		'url': 'https://github.githubassets.com/favicons/favicon.png',
+		'pc': 0.89,
+		'mobile': 0.9
+	},
+});
+SEARCH_ENGINE_MAP.set('bilibili', {
+	'url': 'https://www.bilibili.com',
+	'search': 'https://search.bilibili.com/all?keyword={0}',
+	'explor': ['https://www.bilibili.com/v/popular/rank/all', 'https://www.bilibili.com/v/popular/all?'],
+	'icon': {
+		'url': 'https://www.bilibili.com/favicon.ico?v=1',
+		'pc': 0.88,
+		'mobile': 0.9
+	},
+});
+SEARCH_ENGINE_MAP.set('weibo', {
+	'url': 'https://www.weibo.com',
+	'search': 'https://s.weibo.com/weibo?q={0}',
+	'explor': ['https://s.weibo.com/top/summary?cate=realtimehot','https://s.weibo.com/top/summary?cate=socialevent'],
+	'icon': {
+		'url': 'https://weibo.com/favicon.ico',
+		'mobile': 0.97
+	},
+});
+SEARCH_ENGINE_MAP.set('toutiao', {
+	'url': 'https://www.toutiao.com',
+	'search': 'https://so.toutiao.com/search?keyword={0}',
+	'explor': ['https://so.toutiao.com/search?keyword=%E6%9C%80%E6%96%B0%E6%B6%88%E6%81%AF'],
+	'icon': {
+		'url': 'https://sf3-cdn-tos.douyinstatic.com/obj/eden-cn/uhbfnupkbps/toutiao_favicon.ico',
+	},
+});
+SEARCH_ENGINE_MAP.set('pocket', {
+	'url': 'https://www.getpocket.com',
+	'search': 'https://getpocket.com/my-list/search?query={0}',
+	'explor': ['https://getpocket.com/zh/explore'],
+	'icon': {
+		'url': 'https://getpocket.com/favicon.ico',
+		'pc': 0.90,
+		'mobile': 0.85
+	},
+});
+SEARCH_ENGINE_MAP.set('ithome', {
+	'url': 'https://www.ithome.com',
+	'search': 'https://www.ithome.com/search/{0}.html',
+	'explor': ['https://m.ithome.com/rankm/'],
+	'icon': {
+		'url': 'https://img.ithome.com/img/soft/ithome.svg',
+		'pc': 0.90,
+		'mobile': 0.8
+	},
+});
 
-			document.getElementById("phoneinfo").innerHTML = content $("#phoneinfo").addClass(style)
+SEARCH_ENGINE_MAP.set('xueqiu', {
+	'url': 'https://xueqiu.com/',
+	'search': 'https://xueqiu.com/k?q={0}',
+	'explor': ['https://xueqiu.com/today'],
+	'icon': {
+		'url': 'https://xqdoc.imedao.com/17aebcfb84a145d33fc18679.ico',
+		'pc': 0.95,
+	},
+});
 
-			console.log("battaryPercent:" + batteryPercent)
+SEARCH_ENGINE_MAP.set('maimai', {
+	'url': 'https://maimai.com/',
+	'search': 'https://maimai.cn/web/search_center?type=feed&query={0}',
+	'explor': ['https://maimai.cn/web/feed_explore'],
+	'icon': {
+		'url': 'https://maimai.cn/static/images/website/icons/logo.png',
+	},
+});
 
-			$.getJSON('https://7re15m3a5ra8w.cfc-execute.bj.baidubce.com/',
-			function(res) {
+// https://coolapk.com/favicon.ico
+// https://xueqiu.com/today
+
+String.prototype.format = function () {
+	var formatted = this;
+	for (var arg in arguments) {
+		formatted = formatted.replace("{" + arg + "}", arguments[arg]);
+	}
+	return formatted;
+};
+
+// search engine to show
+var ENGINES = ['baidu', 'bing_cn', 'yandex', 'google', 'github', 'bilibili', 'weibo', 'toutiao', 'pocket', 'zhihu', 'ithome','xueqiu']
+var DEFAULT_ENGINE = 'baidu'
+var SEARCH_INPUT = 'search-input'
+
+function searchEngine(type) {
+	let content = document.getElementById(SEARCH_INPUT).value;
+	let angry = content.endsWith('!') || content.endsWith('！')
+	if (!content){
+		window.location.href = SEARCH_ENGINE_MAP.get(type)['url']
+	} else if (angry) {
+		content = content.replace(/!|！/g, '')
+		for (i in ENGINES) {
+			e = ENGINES[i];
+			if (SEARCH_ENGINE_MAP.has(e)) {
+				window.open(SEARCH_ENGINE_MAP.get(e)['search'].format(content))
+			}
+		}
+	} else {
+		window.location.href = SEARCH_ENGINE_MAP.get(type)['search'].format(content)
+	}
+}
+
+// search contents
+
+function showSearchEngine() {
+	content = ''
+	
+	for (i in ENGINES) {
+		let icoSize = 2.5
+		if (isMobile()) {
+			icoSize = 8
+		} 
+		let type = ENGINES[i]
+		customRatio = isMobile() ?  SEARCH_ENGINE_MAP.get(type)['icon']['mobile'] : SEARCH_ENGINE_MAP.get(type)['icon']['pc']
+		if(!customRatio){
+			customRatio = 1
+		}
+		icoSize = icoSize * customRatio
+		content = content + '<input type="image" onclick=window.searchEngine("{0}") \
+		src=\'./images/icon/{1}.ico\' style="width:{2}%" />'.format(type,type, icoSize) + ' '
+		if(isMobile() && i % 5 == 0){
+			content = content + "<br>"
+		}
+		
+	}
+	document.getElementById('searhers').innerHTML = content;
+
+	document.getElementById(SEARCH_INPUT).onkeypress = function (event) {
+		if (event.which === 13) {
+			searchEngine(DEFAULT_ENGINE)
+		}
+	}
+}
+
+// battary
+window.onload = function () {
+	// https://blog.csdn.net/animatecat/article/details/85164093
+	var isCharge;
+	var batteryPercent = 0;
+	navigator.getBattery().then(function (battery) {
+		isCharge = battery.charging;
+		batteryPercent = Math.round(battery.level * 100);
+		var batteryInfoStyle;
+		if (batteryPercent >= 85) {
+			batteryInfoStyle = "b-green"
+		} else if (batteryPercent > 30) {
+			batteryInfoStyle = "b-orange"
+		} else {
+			batteryInfoStyle = "b-red"
+		}
+		if (!batteryPercent) {
+			batteryPercent = "未知";
+			batteryInfoStyle = "b-orange"
+		}
+		if (isCharge) {
+			style = "b-charging"
+		}
+		var content = batteryPercent + "%"
+		document.getElementById("phoneinfo").innerHTML = content
+		$("#phoneinfo").addClass(style).addClass("battery")
+
+		console.log("battaryPercent:" + batteryPercent)
+
+
+		$.get('https://baidu.com/',
+			function (res) {
 				console.log(res)
 			})
+	});
 
-			$.get('https://baidu.com/',
-			function(res) {
-				console.log(res)
-			})
-		});
+	showSearchEngine();
+};
+
+function takeYourLife() {
+	explors = []
+	values = SEARCH_ENGINE_MAP.values()
+	for(const e of values) {
+		explors = explors.concat(e['explor'])
 	}
 
-	// https://baijiahao.baidu.com/s?id=1707034727158253423&wfr=spider&for=pc
-	const serach_engine_map = new Map();
-	serach_engine_map.set('baidu', ['https://www.baidu.com/s?word=']);
-	serach_engine_map.set('bing_cn', ['https://cn.bing.com/search?q=']);
-	serach_engine_map.set('yandex', ['https://yandex.com/search/?text=']);
-	serach_engine_map.set('zhihu', ['https://www.zhihu.com/search?type=content&q=']);
-	serach_engine_map.set('google', ['https://www.google.com/search?q=']);
-	function search(type) {
-		var content = document.getElementById("search-content").value;
-		window.location.href = serach_engine_map.get(type)[0] + content
-	}
-	function search_image(type) {
-		var content = document.getElementById("search-content").value;
-		return serach_engine_map.get(type)[1]
-	}
-</script>
+	console.log(explors)
+	window.location.href = explors[Math.floor(Math.random() * explors.length)]
+}
+
+function isMobile(){
+    let details = navigator.userAgent;
+	let regexp = /android|iphone|kindle|ipad|mobile|weixin/i;
+    return regexp.test(details);
+}
